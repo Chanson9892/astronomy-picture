7ebedf483a6ba8a3ba8a9813c379bf71a444ec0f
Chanson9892 <chan.hanson5@gmail.com>
2021-03-24 16:39:11 -0700

    shows apod data on to page, can select past dates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
 
-const BASE_URL = 'http://localhost:3000'
+const API = 'http://localhost:3000'
 
-function App() {
-  function getData(url){
-
+class App extends Component{
+  state = {
+    explanation: "explanation here",
+    date: "date here",
+    copyright: "copyright goes here",
+    imageUrl: ""
+  };
+  componentDidMount() {
+    fetch(API + `/get_image`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const { explanation, date, url, copyright } = data;
+        this.setState({
+          explanation: explanation,
+          date: date,
+          imageUrl: url,
+          copyright: copyright,
+        });
+      });
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleChange = (e) => {
+    fetch(API + `/get_image?date=${e.target.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const { explanation, date, url, copyright } = data;
+        this.setState({
+          explanation: explanation,
+          date: date,
+          imageUrl: url,
+          copyright: copyright,
+        });
+      });
+  };
+  render() {
+    const { imageUrl, explanation, date, copyright } = this.state;
+    return (
+      <div style={{ position: "fixed" }}>
+        <h1>{copyright}</h1>
+        <h6>{date}</h6>
+        <p>{explanation}</p>
+        <img
+          style={{ maxHeight: "500px" }}
+          src={imageUrl}
+          alt="this will be a cool pic"
+        />
+        <br />
+        <input type="date" onChange={this.handleChange} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
