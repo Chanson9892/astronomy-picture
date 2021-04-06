cf022ec0ad61dfe3a777e4b54a4a4c9a75a3aadf
Chanson9892 <chan.hanson5@gmail.com>
2021-04-05 18:04:44 -0700

    making progress on showing video

--- src/AstronomyCard.js
+++ src/AstronomyCard.js
@@ -1,18 +1,33 @@
 import React from "react";
 
 const AstronomyCard = (props) => {
-    console.log(props)
+  console.log(props)
     const { title, imageUrl, explanation, date, copyright } = props
+    function IsImage() {
+      if(imageUrl.includes('jpg')){
+        return (
+          <img
+          style={{ maxHeight: "500px" }}
+          src={imageUrl}
+          alt={imageUrl}
+        />
+        ) 
+      } else if (!imageUrl.includes('jpg')){
+        return (
+          <video className="video" controls preload="auto" height="500">
+            <source src={imageUrl} type="video/mp4"/>
+          </video>
+        )
+      } else {
+        return (<h1>No Image found</h1>)
+      }
+    }
     return (
         <div className="astronomyCard">
         <h1>{title}</h1>
         <h3>{date}</h3>
         <br></br>
-        <img
-          style={{ maxHeight: "500px" }}
-          src={imageUrl}
-          alt={imageUrl}
-        />
+        <IsImage />
         <br></br>
         <h1>{copyright}</h1>
         <br></br>
